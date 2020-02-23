da7acae6795b4f9a6d7ed5ff0ebf13f18c2bca43
yanagswk <yanagisawa3939jp@yahoo.co.jp>
2020-02-23 23:05:30 +0900

    タイトルとヘッダー変更

--- journal/models.py
+++ journal/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Journal(models.Model):
-    """日記モデル"""
 
     user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT)
     title = models.CharField(verbose_name='タイトル', max_length=40)
