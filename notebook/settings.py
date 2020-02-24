74c00e5d07188fc2418afdc1963c265bda7c1d71
yanagswk <yanagisawa3939jp@yahoo.co.jp>
2020-02-24 12:30:53 +0900

    settings調整

--- notebook/settings.py
+++ notebook/settings.py
@@ -20,46 +20,7 @@ ALLOWED_HOSTS = []
 
 
 
-# ロギング設定
-LOGGING = {
-    'version': 1,  # 1固定
-    'disable_existing_loggers': False,
 
-    # ロガーの設定
-    'loggers': {
-        # Djangoが利用するロガー
-        'django': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        },
-        # journalアプリケーションが利用するロガー
-        'journal': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
-
-    # ハンドラの設定
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'dev'
-        },
-    },
-
-    # フォーマッタの設定
-    'formatters': {
-        'dev': {
-            'format': '\t'.join([
-                '%(asctime)s',
-                '[%(levelname)s]',
-                '%(pathname)s(Line:%(lineno)d)',
-                '%(message)s'
-            ])
-        },
-    }
-}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
